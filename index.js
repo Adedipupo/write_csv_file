4cf0c51108a62d48191c22e3970b66aa8f0fdaab
Adedipupo <ajokanola@terragonltd.com>
2023-01-18 18:33:08 +0100

    check-age-category

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 import {appendFileSync} from 'fs';
 import { data } from './data.js';
 
-const Contact = (name,phone,email) => {
-    const csv = `${name},${phone},${email}\n`;
+const Contact = (id,name,phone,email,age,category) => {
+    const csv = `${id},${name},${phone},${email},${age},${category}\n`;
     try {
         appendFileSync("./contacts.csv",csv)
-
     } catch (error) {
         console.log(error)
     }
 }
 
-console.log(data);
-//testting
+const result = data.map(contact => contact);
+
+function checkCategory(num){
+    if (num > 0 && num <= 25){
+        return 'Young Blud'
+   }else if (num >25 && num <=45){
+        return 'Mature'
+   }else{
+        return 'Agba !!!'
+   }    
+}
+
 
 const startApp = () => {
-    const contact1 = Contact("Bill", "+00123456789", "bill@codingthesmartway.com");
-    return contact1;
+    result.forEach(contact => {
+        const {id,name,phone,email,age} = contact;
+        const contact1 = Contact(id,name,phone,email,age,checkCategory(age));
+        return contact1;
+    })
 }
 
 startApp();
