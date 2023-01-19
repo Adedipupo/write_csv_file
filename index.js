642a3ab2ff008a073e432b784aa4ba5b22e7c998
Adedipupo <ajokanola@terragonltd.com>
2023-01-19 10:46:13 +0100

    json-placeholder-api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import {appendFileSync} from 'fs';
-import { data } from './data.js';
+import axios from 'axios';
 
-const Contact = (id,name,phone,email,age,category) => {
-    const csv = `${id},${name},${phone},${email},${age},${category}\n`;
+const Contact = (id,name,username,email,address) => {
+    const csv = `${id},${name},${username},${email},${address}\n`;
     try {
         appendFileSync("./contacts.csv",csv)
     } catch (error) {
@@ -10,23 +10,28 @@ const Contact = (id,name,phone,email,age,category) => {
     }
 }
 
+ const users = await axios.get("https://jsonplaceholder.typicode.com/users");
+ const data = users.data;
+
 const result = data.map(contact => contact);
 
-function checkCategory(num){
-    if (num > 0 && num <= 25){
-        return 'Young Blud'
-   }else if (num >25 && num <=45){
-        return 'Mature'
-   }else{
-        return 'Agba !!!'
-   }    
-}
+// function checkCategory(num){
+//     if (num > 0 && num <= 25){
+//         return 'Young Blud'
+//    }else if (num >25 && num <=45){
+//         return 'Mature'
+//    }else{
+//         return 'Agba !!!'
+//    }    
+// }
 
 
 const startApp = () => {
     result.forEach(contact => {
-        const {id,name,phone,email,age} = contact;
-        const contact1 = Contact(id,name,phone,email,age,checkCategory(age));
+        const {id,name,username,email} = contact;
+        const {street,city,zipcode} = contact.address;
+        const address = `${street},${city},${zipcode}`;
+        const contact1 = Contact(id,name,username,email,address);
         return contact1;
     })
 }
